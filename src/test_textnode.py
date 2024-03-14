ba56f8b2ad924023b9091038823ee59272843047
mikeheiberger <michael.heiberger@gmail.com>
2024-03-13 22:09:01 -0600

    Adds TextNode tests

---
+++ src/test_textnode.py
@@ -0,0 +1,22 @@
+import unittest
+
+from textnode import TextNode
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_different_text_type(self):
+        node = TextNode("Test", "bold", "https://www.google.com")
+        node2 = TextNode("Test", "underline", "https://www.google.com")
+        self.assertNotEqual(node, node2)
+    
+    def test_different_text(self):
+        node = TextNode("Test1", "bold")
+        node2 = TextNode("Test2", "bold")
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,10 +1,10 @@
 class TextNode:
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
-    def eq(left, right):
+    def __eq__(left, right):
         return (left.text == right.text and
                 left.text_type == right.text_type and
                 left.url == right.url)
