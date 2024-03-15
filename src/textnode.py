a90eb86793d2823ca9df160a13e74b208de98a75
mikeheiberger <michael.heiberger@gmail.com>
2024-03-14 23:58:34 -0600

    Moves text_node to html_node conversion to textnode file

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,12 @@
+from leafnode import LeafNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -11,3 +20,19 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == text_type_text:
+        return LeafNode(text_node.text)
+    elif text_node.text_type == text_type_bold:
+        return LeafNode(text_node.text, "b")
+    elif text_node.text_type == text_type_italic:
+        return LeafNode(text_node.text, "i")
+    elif text_node.text_type == text_type_code:
+        return LeafNode(text_node.text, "code")
+    elif text_node.text_type == text_type_link:
+        return LeafNode(text_node.text, "a", { "href": text_node.url })
+    elif text_node.text_type == text_type_image:
+        return LeafNode("", "img", { "src": text_node.url, "alt": text_node.text })
+    else:
+        raise Exception(f"Could not handle text node of type {text_node.text_type}")
