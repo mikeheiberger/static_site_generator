061386aac42f5d72c69aec01aa04329b3b1873e5
Mike Heiberger <michael.heiberger@gmail.com>
2024-04-03 00:09:38 -0600

    Adds block to block type function

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,65 @@
 import re
 
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
+def block_to_block_type(block: str) -> str:
+    # Heading test
+    heading_matched = re.match(r"#{1,6}", block)
+    if heading_matched:
+        return block_type_heading
+
+    # Code test
+    code_matched = re.match(r"```([\s\S]*?)```", block)
+    if code_matched:
+        return block_type_code
+
+    # Quote test
+    lines = block.splitlines()
+    all_lines_are_quotes = True
+    for line in lines:
+        if not line.startswith(">"):
+            all_lines_are_quotes = False
+            break
+    if all_lines_are_quotes:
+        return block_type_quote
+
+    # Asterisk unordered list
+    all_lines_start_asterisk = True
+    for line in lines:
+        if not line.startswith("*"):
+            all_lines_start_asterisk = False
+            break
+    if all_lines_start_asterisk:
+        return block_type_unordered_list
+
+    # Dash unordered list
+    all_lines_start_dash = True
+    for line in lines:
+        if not line.startswith("-"):
+            all_lines_start_dash = False
+            break
+    if all_lines_start_dash:
+        return block_type_unordered_list
+
+    # Ordered list
+    if lines[0].startswith("1."):
+        is_ordered_list = True
+        for i in range (1, len(lines)):
+            if not lines[i].startswith(f"{i + 1}."):
+                is_ordered_list = False
+                break
+        if is_ordered_list:
+            return block_type_ordered_list
+    
+    # Otherwise its a paragraph
+    return block_type_paragraph
+    
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     stripped_blocks = []
     blocks = re.split(r"(?:\r?\n){2,}", markdown)
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,14 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import (
+    block_type_paragraph,
+    block_type_heading,
+    block_type_code,
+    block_type_quote,
+    block_type_unordered_list,
+    block_type_ordered_list,
+    markdown_to_blocks,
+    block_to_block_type
+)
 
 class TestMarkdownToHTML(unittest.TestCase):
     def test_markdown_blocks(self):
@@ -47,6 +56,65 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ]
         )
+
+    def test_block_to_block_type_heading(self):
+        heading = "### This is a heading"
+        block_type = block_to_block_type(heading)
+        self.assertEqual(block_type, block_type_heading)
+    
+    def test_block_to_block_type_code(self):
+        code = """```Here is some code.
+        It spans multiple lines.
+        Three lines, actually.```"""
+        block_type = block_to_block_type(code)
+        self.assertEqual(block_type, block_type_code)
+
+    def test_block_to_block_type_quote(self):
+        quote = """>Here is a quote block.
+>It spans multiple lines.
+>Three lines, actually."""
+        block_type = block_to_block_type(quote)
+        self.assertEqual(block_type, block_type_quote)
+
+    def test_block_to_block_type_asterisk_unordered_list(self):
+        unordered_list = """*Here is an unordered list
+*It spans multiple lines
+*Three lines, actually
+*Now it is four"""
+        block_type = block_to_block_type(unordered_list)
+        self.assertEqual(block_type, block_type_unordered_list)
+
+    def test_block_to_block_type_dash_unordered_list(self):
+        unordered_list = """-Here is an unordered list
+-It spans multiple lines
+-Three lines, actually
+-Now it is four"""
+        block_type = block_to_block_type(unordered_list)
+        self.assertEqual(block_type, block_type_unordered_list)
+
+    def test_block_to_block_type_mixed_unordered_list(self):
+        unordered_list = """-Here is an unordered list
+*It spans multiple lines
+-Three lines, actually
+-Now it is four"""
+        block_type = block_to_block_type(unordered_list)
+        self.assertEqual(block_type, block_type_paragraph)
+    
+    def test_block_to_block_type_ordered_list(self):
+        ordered_list = """1. First thing
+2. Second thing
+3. Third thing
+4. Fourth thing"""
+        block_type = block_to_block_type(ordered_list)
+        self.assertEqual(block_type, block_type_ordered_list)
+
+    def test_block_to_block_type_ordered_list_broken(self):
+        ordered_list = """1. First thing
+2. Second thing
+2. Third thing
+4. Fourth thing"""
+        block_type = block_to_block_type(ordered_list)
+        self.assertEqual(block_type, block_type_paragraph)
 
 if __name__ == "__main__":
     unittest.main()
