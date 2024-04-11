bc0ee2e40bdb3891da821c4b75c19c866e7a4b1e
mikeheiberger <michael.heiberger@gmail.com>
2024-04-10 23:00:32 -0600

    Adds direcotry copying from new static directory to public directory

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
-from textnode import TextNode
+import os
+import shutil
+
+def copy_directory(source: str, dest: str):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    if not os.path.exists(source):
+        raise ValueError("Incorrect source path")
+    os.mkdir(dest)
+    source_children = os.listdir(source)
+    for child in source_children:
+        current_source = os.path.join(source, child)
+        current_dest = os.path.join(dest, child)
+        if os.path.isfile(current_source):
+            shutil.copy(current_source, current_dest)
+        else:
+            copy_directory(current_source, current_dest)
 
 def main():
-    text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(text_node)
-
+    copy_directory("static", "public")
 
 main()
