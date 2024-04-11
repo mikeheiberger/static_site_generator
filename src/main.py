bbde9f13afd524bd2aa57a7fb01fab6446c2907c
mikeheiberger <michael.heiberger@gmail.com>
2024-04-10 23:04:49 -0600

    Makes some small improvements to the direcotry copying code

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 import os
 import shutil
 
+dir_path_static = "./static"
+dir_path_public = "./public"
+
 def copy_directory(source: str, dest: str):
-    if os.path.exists(dest):
-        shutil.rmtree(dest)
     if not os.path.exists(source):
         raise ValueError("Incorrect source path")
-    os.mkdir(dest)
-    source_children = os.listdir(source)
-    for child in source_children:
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    for child in os.listdir(source):
         current_source = os.path.join(source, child)
         current_dest = os.path.join(dest, child)
         if os.path.isfile(current_source):
@@ -17,6 +18,8 @@ def copy_directory(source: str, dest: str):
             copy_directory(current_source, current_dest)
 
 def main():
-    copy_directory("static", "public")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+    copy_directory(dir_path_static, dir_path_public)
 
 main()
